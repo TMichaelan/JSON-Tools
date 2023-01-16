b0c4e01ae99a2f89e3faba10c1605a65e2bed247
Michael <bigapplematthias@gmail.com>
2023-01-16 19:17:36 +0100

    fix: html, maven??

---
+++ target/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"pl.put.poznan.transformer.app"},{"l":"pl.put.poznan.transformer.logic"},{"l":"pl.put.poznan.transformer.logic.domain"},{"l":"pl.put.poznan.transformer.logic.tools"},{"l":"pl.put.poznan.transformer.rest"}]
